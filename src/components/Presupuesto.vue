cf05eb5cf89087d443bf2c3e5f1a40497eb6a849
Orlando Javier <oj.flores118@gmail.com>
2024-02-15 22:15:11 -0600

    Validando presupuesto

--- src/components/Presupuesto.vue
+++ src/components/Presupuesto.vue
@@ -1,15 +1,29 @@
 <script setup>
+    import { ref } from 'vue'
 
+    const presupuesto = ref(0)
+
+    const definirPresupuesto = () => {
+        if(presupuesto <= 0) {
+            console.log('Presupuesto no valido')
+        }
+    }
 </script>
 
 <template>
-    <form action="">
+    <form 
+        class="presupuesto"
+        @submit.prevent="definirPresupuesto"
+    >
         <div class="campo">
             <label for="nuevo-presupuesto">Definir presupuesto</label>
             <input 
                 id="nuevo-presupuesto" 
                 class="nuevo-presupuesto"
                 placeholder="Añade un presupuesto"
+                type="number"
+                min="0"
+                v-model.number="presupuesto"
             >
         </div>
 
@@ -18,5 +32,41 @@
 </template>
 
 <style scoped>
-
+.presupuesto {
+    width: 100% ;
+}
+.campo {
+    display: grid;
+    gap: 2rem;
+}
+.presupuesto label {
+    font-size: 2.5rem;
+    text-align: center;
+    color: var(--azul);
+}
+.presupuesto input[type="number"]{
+    background-color: var(--gris-claro);
+    border-radius: 1rem;
+    padding: 1rem;
+    border: none;
+    font-size: 2.2rem;
+    text-align: center;
+}
+.presupuesto input[type="submit"]{
+    background-color: var(--azul);
+    border-radius: 1rem;
+    border: none;
+    padding: 1rem;
+    text-align: center;
+    font-size: 2rem;
+    margin-top: 2rem;
+    color: var(--blanco);
+    font-weight: 900;
+    width: 100%;
+    transition: background-color 300ms ease;
+}
+.presupuesto input[type="submit"]:hover {
+    background-color: #1048A4;
+    cursor: pointer;
+}
 </style>
