0ff8e5cfd98bfbc73a90d11366258573fd4a5103
Orlando Javier <oj.flores118@gmail.com>
2024-03-13 21:02:06 -0600

    Primeros pasos mostrando gastos

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,11 @@
   import { ref, reactive } from 'vue'
   import Presupuesto from './components/Presupuesto.vue';
   import ControlPresupuesto from './components/ControlPresupuesto.vue';
+  import Gasto from './components/Gasto.vue'
   import Modal from './components/Modal.vue';
+  import { generarId } from './helpers'
   import iconoNuevoGasto from './assets/img/nuevo-gasto.svg';
+import GastoVue from './components/Gasto.vue';
 
 
   const modal = reactive({
@@ -17,7 +20,8 @@
         id: null,
         fecha: Date.now()
     })
-
+  
+  const gastos = ref([]) //Arreglo vacío
   const presupuesto = ref(0)
   const disponible = ref(0)
 
@@ -37,6 +41,24 @@
     setTimeout(() => {
       modal.mostrar = false
     }, 300)
+  }
+
+  const guardarGasto = () => {
+    gastos.value.push({
+      ...gasto,
+      id: generarId()
+    })
+
+    ocultarModal()
+
+    // Reiniciar el objeto
+    Object.assign(gasto, {
+      nombre: '',
+      cantidad: '',
+      categoria: '',
+      id: null,
+      fecha: Date.now()
+    })
   }
 </script>
 
@@ -59,6 +81,16 @@
 
     <main v-if="presupuesto > 0">
 
+      <div class="listado-gastos contenedor">
+        <h2> {{ gastos.length > 0 ? 'Gastos' : 'No hay gastos' }} </h2>
+
+        <Gasto
+          v-for="gasto in gastos"
+          :key="gasto.id"
+          gasto="gasto"
+        />
+      </div>
+
       <div class="crear-gasto">
         <img 
           :src="iconoNuevoGasto"
@@ -70,6 +102,7 @@
       <Modal 
         v-if="modal.mostrar"
         @ocultar-modal="ocultarModal"
+        @guardar-gasto="guardarGasto"
         :modal="modal"
         v-model:nombre="gasto.nombre"
         v-model:cantidad="gasto.cantidad"
@@ -144,5 +177,13 @@
   .crear-gasto img {
     width: 5rem;
     cursor: pointer;
+  }
+
+  .listado-gastos {
+    margin-top: 10rem;
+  }
+  .listado-gastos h2 {
+    font-weight: 700;
+    color: var(--gris-oscuro);
   }
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -4,7 +4,7 @@
     import cerrarModal from '../assets/img/cerrar.svg'
 
     const error = ref('')
-    const emit = defineEmits(['ocultar-modal', 'update:nombre', 'update:cantidad', 'update:categoria'])
+    const emit = defineEmits(['ocultar-modal', 'guardar-gasto', 'update:nombre', 'update:cantidad', 'update:categoria'])
     const props = defineProps({
         modal: {
             type: Object,
@@ -45,7 +45,7 @@
             return 
        }
 
-       console.log('Emitiendo gasto..')
+       emit('guardar-gasto')
         
     }
 </script>
